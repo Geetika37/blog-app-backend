cd1b23aacc2ea26bb3ca128d32254db5cde561e6
Geetika37 <geetikadinesh3@gmail.com>
2024-02-09 10:12:52 +0530

    created

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -3,7 +3,13 @@ const userModel=require("../models/userModel")
 const router=express.Router()
 
 router.post("/sign_up",async(req,res)=>{
-    
+    let data=req.body
+    let user=new userModel(data)
+    let result=await user.save()
+
+    res.send({
+        status:"success"
+    })
 })
 
 module.exports=router
